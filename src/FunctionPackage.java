58323829366a194f658551cb4ba8ba733a9702a6
Archimonde <gilbert_v@yeah.net>
2020-04-25 22:01:53 +0800

    add list

--- src/FunctionPackage.java
+++ src/FunctionPackage.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FunctionPackage {
     public static void main(String[] args){
-        System.out.println("java");
+
+        int[] listes = {91, 82, 34, 46, 53,10};
+        System.out.println("排序前的id：" + listes);
+        Arrays.sort(listes);
+
+        for(int i = 0; i < listes.length; i++){
+            // 遍历当前数组
+            System.out.println(listes[i]);
+
+        }
+        System.out.println("排序后的id：" + listes);
+        List <Integer> liste = new ArrayList<Integer>();
+        liste.add(56);
+        liste.add(89);
+        liste.add(139);
+
+        System.out.println(liste);
     }
 }
