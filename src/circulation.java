36c593c8877e7c780990d44d5fdb946abb137c96
Archimonde <gilbert_v@yeah.net>
2020-08-24 14:42:42 +0800

    Add function and Interface

---
+++ src/circulation.java
@@ -0,0 +1,20 @@
+public class circulation {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+
+    }
+
+    public void max(){
+        System.out.println("void");
+    }
+
+    public void max(int a, int b){
+        if (a > b){
+            System.out.println("The max is " + a);
+        }
+        else {
+            System.out.println("The max is "+ b);
+        }
+    }
+}
---
+++ src/practice.java
@@ -0,0 +1,5 @@
+public interface practice {
+    public interface PracticeTime{
+        
+    }
+}
