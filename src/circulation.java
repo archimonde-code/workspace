221dbf15a7ffeef5cd0cdbddf84cda24ee3043da
Archimonde <gilbert_v@yeah.net>
2020-10-20 16:03:13 +0800

    update circulation

--- src/circulation.java
+++ src/circulation.java
@@ -2,19 +2,7 @@ public class circulation {
     public static void main(String[] args){
         int a = 10;
         int b = 20;
+        System.out.println(a + b);
 
-    }
-
-    public void max(){
-        System.out.println("void");
-    }
-
-    public void max(int a, int b){
-        if (a > b){
-            System.out.println("The max is " + a);
-        }
-        else {
-            System.out.println("The max is "+ b);
-        }
     }
 }
