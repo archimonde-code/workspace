3063a4d29847e32a62b720749ad422b47b815d94
Archimonde <gilbert_v@yeah.net>
2020-10-26 14:46:05 +0800

    新增数组相关联系，其中包含冒泡和选择排序两种算法练习

--- src/circulation.java
+++ src/circulation.java
@@ -1,58 +1,58 @@
 public class circulation {
     public static void main(String[] args){
-
+        WhileCirculation while_method = new WhileCirculation();
+        while_method.whileCirculation();
     }
-    public class WhileCirculation{
-        public void whileCirculation(){
-            int sum = 0;
-            int i = 1;
-            while (i <= 10){
-                sum = sum + i;
-                i++;
-            }
-            System.out.println("1~10的累加之和为：" + sum);
+}
+class WhileCirculation{
+    public void whileCirculation(){
+        int sum = 0;
+        int i = 1;
+        while (i <= 10){
+            sum = sum + i;
+            i++;
         }
-
-        public int circulation(){
-            int x = 1;
-            int count = 0;
-            while (x <= 100){
-                if(x % 6 == 0){
-                    System.out.println("count=" + count);
-                }
-            }
-            return count;
-        }
+        System.out.println("1~10的累加之和为：" + sum);
     }
-    public class ForCirculation{
-        public void forCirculation(){
-            //打印5行递增三角形的*号图案
-            for(int x = 1; x <= 5; x++){
-                for (int y = 1; y <= x; y++){
-                    System.out.print("*");
-                }
-                System.out.println();
+
+    public int circulation(){
+        int x = 1;
+        int count = 0;
+        while (x <= 100){
+            if(x % 6 == 0){
+                count++;
             }
-            //-----------------------------------------
-            //打印九九乘法表
-            for (int x = 1; x < 10; x++){
+            x++;
+        }
+        return count;
+    }
+}
+class ForCirculation{
+    public void forCirculation(){
+        //打印5行递增三角形的*号图案
+        for(int x = 1; x <= 5; x++){
+            for (int y = 1; y <= x; y++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //-----------------------------------------
+        //打印九九乘法表
+        for (int x = 1; x < 10; x++){
             for (int y = 1; y <= x; y++){
                 System.out.print(y + " * " + x + " = " + (x * y) + "\t");
             }
             System.out.println();
-            }
-            //打印倒三角形
-            for(int x = 1; x <= 5; x++){
-                for(int y = 1; y < x; y++){
-                    System.out.print(" ");
-                }
-                for (int z = x; z<=5; z++){
-                    System.out.print("* ");
-                }
-                System.out.println();
         }
-
+        //打印倒三角形
+        for(int x = 1; x <= 5; x++){
+            for(int y = 1; y < x; y++){
+                System.out.print(" ");
+            }
+            for (int z = x; z<=5; z++){
+                System.out.print("* ");
+            }
+            System.out.println();
         }
     }
 }
-
---
+++ src/code/ArrayList.java
@@ -0,0 +1,82 @@
+package code;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+
+public class ArrayList {
+    public static void main(String args[]){
+        int[] myArrayList = {10, 9, 15, 8, 24, 36, 42, 56, 1, 99};
+        System.out.println("选择排序：");
+        arraySort(myArrayList);
+        traverse(myArrayList);
+        System.out.println();
+        System.out.println("冒泡排序");
+        bubbleSort(myArrayList);
+        traverse(myArrayList);
+        System.out.println();
+        Arrays.sort(myArrayList);
+        traverse(myArrayList);
+        doubleArray();
+    }
+    /*
+    * 遍历数组*/
+    public static void traverse(int[] myArray){
+        for(int i = 0; i < myArray.length; i++){
+            System.out.print(myArray[i] + " ");
+        }
+    }
+    /*
+    * 获取数组最大值*/
+    public static int arrayList(int @NotNull [] myArray){
+        int max = myArray[0];
+        for(int i = 1; i < myArray.length; i++){
+            System.out.println("myArrayList["+ i + "]" + "=" +myArray[i]);
+            if (myArray[i] > max) {
+                max = myArray[i];
+            }
+        }
+        return max;
+    }
+
+    /*
+    * 选择排序*/
+    public static int[] arraySort(int[] myArray){
+        int[] copyArray = Arrays.copyOf(myArray, myArray.length);
+        for(int x = 0; x < myArray.length - 1; x++){
+            int min = x;
+            for (int y = x + 1; y < myArray.length; y++){
+                if (myArray[min] > myArray[y]){
+                    min = y;
+                }
+            }
+            if (myArray[x] > myArray[min]){
+                int temp = myArray[x];
+                myArray[x] = myArray[min];
+                myArray[min] = temp;
+            }
+        }
+        return copyArray;
+    }
+    /*
+    * 冒泡排序*/
+    public static void bubbleSort(int[] myArray){
+        for (int i = 0; i < myArray.length - 1; i++){
+            for (int x = 0; x < myArray.length - 1 - i; x++){
+                if(myArray[x] > myArray[x + 1]){
+                    int temp = myArray[x];
+                    myArray[x] = myArray[x + 1];
+                    myArray[x + 1] = temp;
+                }
+            }
+        }
+    }
+    public static void doubleArray(){
+        int[][] arrays = {{1,2,3,4,5},{5,4,3,2,1},{6,7,8,9,10},{10, 9, 8, 7, 6, 5}};
+        for (int x = 0; x < arrays.length; x++){
+            for (int y = 0; y < arrays[1].length; y++){
+                System.out.print(arrays[x][y] + " ");
+            }
+        }
+    }
+}
